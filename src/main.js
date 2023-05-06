91fed0006f1847d67d35a88b6032daa6276f2d9b
Yan Matheus <95008410+REFSZIN@users.noreply.github.com>
2023-05-06 00:44:15 -0300

    Feat:  🚀 Checkout  &&  Home Products  Implemented

--- src/main.js
+++ src/main.js
@@ -4,11 +4,13 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
+import VueTheMask from 'vue-the-mask';
 
 loadFonts()
 
 createApp(App)
   .use(router)
   .use(store)
+  .use(VueTheMask)
   .use(vuetify)
   .mount('#app')
